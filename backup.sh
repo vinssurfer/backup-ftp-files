cdcb64e1b500b943acc44d3f2d65acbc9b5cad7b
vinssurfer <vinssurfer@yahoo.fr>
2017-08-02 17:18:22 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Check sendemail installation
+sudo apt install sendemail -y
+
+# Constants you need to modify
+HOST=serveur_ftp
+LOGIN=login
+PASSWORD=password
+PORT=21
+SAVE_PATH=/home/sav
+LOG_FILE=/var/log/backup.log
+EMAIL=zzzz.wwww@xxx.com
+SMTP=smtp.xxx.com
+BACKUP_TITLE=My backup
+
+# Go to the save path
+cd $SAVE_PATH
+
+# Recursive copy of files until 20 levels
+wget --recursive --level=20 --output-file=$LOG_FILE ftp://$LOGIN:$PASSWORD@$HOST
+
+# Send the mail with the log file
+sendemail -f $EMAIL -t $EMAIL -u Backup $BACKUP_TITLE -m Fin bakup $BACKUP_TITLE -s $SMTP -a $LOG_FILE
